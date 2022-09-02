35f2f2efb3f048d7dd6a6444e31357d0b45582b6
kellsnc <kellspam@outlook.fr>
2022-09-02 02:09:20 +0200

    Remove car fix (now in the mod loader)

--- SA2-Fixed-Edition/mod.cpp
+++ SA2-Fixed-Edition/mod.cpp
@@ -37,42 +37,6 @@ void Remove_SA2ErrorMSG()
     WriteData<5>((void*)0x439a66, 0x90);
 }
 
-// Add additional safety checks for sub_5E3D90 to fix NaN car positions
-BOOL CalcCarPath_r(int a1, LoopHead* path, float* a3, float* a4, float a5, NJS_POINT3* out_pos, Angle3* out_ang)
-{
-    NJS_POINT3 out1, out2;
-
-    if (sub_5E3D90(path, a1, a3, a5, &out1) ||
-        sub_5E3D90(path, a1, a3, a4[15], &out1) ||
-        sub_5E3D90(path, a1, a3, a4[16], &out2)) {
-        return TRUE;
-    }
-
-    NJS_POINT3 vec;
-    vec.x = out1.x - out2.x;
-    vec.y = out1.y - out2.y;
-    vec.z = out1.z - out2.z;
-
-    float dist = vec.x * vec.x + vec.z * vec.z;
-
-    if (dist <= 0.0f)
-        dist = 0.0f;
-
-    out_ang->x = NJM_RAD_ANG(atan2f(-vec.y, sqrtf(dist)));
-    out_ang->y = NJM_RAD_ANG(atan2f(vec.x, vec.z));
-    out_ang->z = 0;
-
-    vec.x = vec.x * 0.5f;
-    vec.y = vec.y * 0.5f;
-    vec.z = vec.z * 0.5f;
-
-    out_pos->x = out2.x + vec.x;
-    out_pos->y = out2.y + vec.y;
-    out_pos->z = out2.z + vec.z;
-
-    return FALSE;
-}
-
 //Sonic Light Dash Afterimage Shadow Bugfix code
 //Credit: End User
 FunctionPointer(void, sub_476C20, (int a1, int a2, int a3, float a4, char a5), 0x476C20);
@@ -152,9 +116,6 @@ extern "C"
 	__declspec(dllexport) void __cdecl Init(const char* path, const HelperFunctions& helperFunctions)
 	{
 		config::read(path);
-
-        // Fix City Escape car bug
-		GenerateUsercallHook(CalcCarPath_r, rEAX, 0x5DE0B0, rECX, rEDX, rEAX, stack4, stack4, stack4, stack4);
 
 		// Fix City Escape light field task missing
 		CityEscape_ObjectArray[58].Function = (ObjectFuncPtr)LIGHT_SW;
